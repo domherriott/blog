e01850930fb4e9f1824fc997b6d8060385b55a51
Dominic Herriott <dherriott@deloitte.co.uk>
2021-01-01 23:52:42 +0000

    Updated content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2021 Dominic Herriott</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a> and <a title="Tim Baker Github" href="https://github.com/tbakerx">Tim Baker</a></li>
            </ul>
 
         </div>
