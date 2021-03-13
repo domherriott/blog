5dfcb6d5c34df992b81ded8490b66989ef1e3579
Dominic Herriott <dherriott@deloitte.co.uk>
2021-03-13 18:26:23 +0000

    Updated text and descriptions

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,9 +7,9 @@ class Contact extends Component {
       var name = this.props.data.name;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      // var state = this.props.data.address.state;
+      // var zip = this.props.data.address.zip;
+      //  var phone= this.props.data.phone;
       // var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
@@ -81,9 +81,8 @@ class Contact extends Component {
 					   <h4>Address and Phone</h4>
 					   <p className="address">
 						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
+						   
+						   {city}
 					   </p>
 				   </div>
 
