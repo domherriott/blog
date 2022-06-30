61d0c487d4894e22e92dc10fc9da9611aa0534ac
domherriott <dherriott@deloitte.co.uk>
2022-06-30 14:40:18 +0100

    updated routes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-// import Contact from './Components/Contact';
-// import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import Otter from './Pages/Otter';
+import { Routes, Route } from 'react-router-dom';
+
 
 class App extends Component {
 
@@ -42,13 +43,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Routes>
+        <Route path="/" element={<>
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
-        {/* <Contact data={this.state.resumeData.main}/> */}
-        <Footer data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/> </>
+             } />
+
+
+          <Route path="/otter" element={<Otter />} />
+        </Routes>
       </div>
     );
   }
---
+++ src/Pages/Otter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Otter(){
+    return(
+
+<div>asdf</div>
+
+//   <header class="container xlarge padding-24">
+//     <a href="#" class="left button white">art of the day</a>
+//     <a href="#about" class="right button white">about</a>
+//   </header>
+
+    // <div class="img center" style="height:100%">
+        // <img src="https://pablo-prod-images-lo-res.s3.eu-west-2.amazonaws.com/image.png" style="width:40%" class="padding-64"></img>
+        // <h2 class="padding-8" id="countdown"></h2>
+    // </div>
+  
+//   <div style="height:250px"></div>
+
+    )
+};
+
+export default Otter;
